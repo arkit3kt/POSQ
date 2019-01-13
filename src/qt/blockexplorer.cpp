f762cde644000fd46aa3496b77c93e2e9cc04666
sicXnull <cvanhauterjr@gmail.com>
2019-01-13 14:24:37 -0500

    Copyright Updates

--- src/qt/blockexplorer.cpp
+++ src/qt/blockexplorer.cpp
@@ -1,3 +1,8 @@
+// Copyright (c) 2015-2018 The PIVX developers
+// Copyright (c) 2018-2019 The POSQ developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #include "blockexplorer.h"
 #include "bitcoinunits.h"
 #include "chainparams.h"
